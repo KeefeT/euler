f6a8e1b386082284cbdaaa7e6aa3500607e59e94
KeefeT <tjk819@gmail.com>
2025-06-04 20:55:37 -0400

    problem 8 python

---
+++ python/problem8.py
@@ -0,0 +1,32 @@
+def main():
+    print("start")
+    arr = []
+    max = -1
+
+    # fill number into array
+    with open("resources/problem8-number.txt") as file:
+        for line in file:
+            for num in line:
+                if num != '\n':
+                    arr.append(int(num))
+
+    # shitty iterator 
+    start = 0
+
+    while start+13 < 1001:
+        temp = 1
+        for i in range(start, start+13):
+            if arr[i] == 0:
+                break
+            temp *= arr[i]
+
+        if temp > max:
+            max = temp
+
+        start += 1
+
+    print("max value: " + str(max))
+    # solution 23514624000
+
+if __name__ == "__main__":
+    main()
